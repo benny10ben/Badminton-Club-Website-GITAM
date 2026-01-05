3ba7df81b58e4003d41343cd7d231029b7682f07
benny10ben <developer.ben@protonmail.com>
2026-01-06 04:55:07 +0530

    Configure gitignore and remove virtual environment

---
+++ README.md
@@ -0,0 +1,1 @@
+# Badminton-Club-Website-GITAM
---
+++ club/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from .models import Sponsers, EventsList, GalleryImages, GalleryHeadings, TeamMembers, TeamHeadings, AlumniMembers, AlumniHeadings
+import datetime
+from django.utils import timezone
+# Create your views here.
+
+def  home(request):
+    sponsers = Sponsers.objects.all()
+    eventslist = EventsList.objects.all().order_by('StartDate')
+    current_date = timezone.now()
+    context = {'sponsers': sponsers, 'eventslist':eventslist, 'current_date': current_date}
+    return render(request,'club/home.html', context)
+
+
+def Events(request):
+    eventslist = EventsList.objects.all().order_by('-StartDate')
+    context = {'eventslist':eventslist}
+    return render(request,'club/Events.html', context)
+
+
+def Gallery(request):
+    galleryimages = GalleryImages.objects.all().order_by('-EventDate')
+    galleryheadings = GalleryHeadings.objects.all().order_by('-EventDate')
+    context = {'galleryimages':galleryimages,'galleryheadings':galleryheadings}
+    return render(request,'club/Gallery.html', context)
+
+
+def  Team(request):
+    teammembers = TeamMembers.objects.all()
+    teamheadings = TeamHeadings.objects.all().order_by('Date')
+    alumnimembers = AlumniMembers.objects.all()
+    alumniheadings = AlumniHeadings.objects.all().order_by('-Date')
+    context = {'teammembers': teammembers, 'teamheadings':teamheadings, 'alumnimembers': alumnimembers, 'alumniheadings':alumniheadings}
+    return render(request,'club/Team.html', context)
+
